9faa2bcfde7598996e696f04f40caee740b16eb6
nathertan <ndihyan@gmail.com>
2022-11-03 13:43:40 +0700

    init commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">    
+    <title>Blog</title>
+
+    <style>
+        h1{
+            color:#037bfb
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">GSLC Ses 5</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+        <li class="nav-item">
+                <a class="nav-link" href="/#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/blog">Blog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/about">About</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
+    <h1>Web Programming</h1>
+    <h2>by: Nathanael Dihyan</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas fugiat esse recusandae. Praesentium quos consequatur repellat, atque, laborum sequi officiis a maxime ipsam distinctio tenetur fuga. Molestias ex at fugiat.</p>
+    
+    <h1>Laravel</h1>
+    <h2>by: Cyanta alias Nathanael Dihyan</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis non aliquid consequatur quam vel beatae placeat veniam sit ea hic a, facere quasi mollitia eligendi odit! Nulla excepturi quisquam adipisci!</p>
+</body>
+</html>
---
+++ resources/views/student.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Student data</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<style>
+    table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
+
+    td, th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+    }
+
+    tr:nth-child(even) {
+      background-color: #dddddd;
+    }
+
+    h1{
+        text-align: center;
+    }
+
+    img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
+
+<body>
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">GSLC Ses 5</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+      <li class="nav-item">
+            <a class="nav-link" href="/#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/about">About</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="/blog">Blog</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    <h1>Group 4</h1>
+    <img src="https://rekreartive.com/wp-content/uploads/2018/10/Logo-Binus-University-Universitas-Bina-Nusantara-Original-PNG-1024x630.png" alt="" width="30%" >
+
+    <table>
+        <tr>
+          <th>NIM</th>
+          <th>Nama</th>
+          <th>Email</th>
+        </tr>
+
+        <tr>
+            <td>2440006186</td>
+            <td>Giovanni Chandra</td>
+            <td>giovanni.chandra@binus.ac.id</td>
+        </tr>
+
+        <tr>
+            <td>2440013134</td>
+            <td>Shannya Maureen M</td>
+            <td>shannya.michelia@binus.ac.id</td>
+        </tr>
+
+        <tr>
+            <td>2440006186</td>
+            <td>Nathanael Dihyan</td>
+            <td>nathanael.dihyan@binus.ac.id</td>
+        </tr>
+
+        <tr>
+            <td>2440094472</td>
+            <td>Vivianit Indyca Vica</td>
+            <td>vivianit.vica@binus.ac.id</td>
+        </tr>
+
+        <tr>
+            <td>2440108332</td>
+            <td>Azizah Nabilah Anwar</td>
+            <td>azizah.anwar@binus.ac.id@binus.ac.id</td>
+
+        </tr>
+    </table>
+</body>
+</html>
